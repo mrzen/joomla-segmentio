646f38738baf8abfe529f8a4faadc794db1a7a30
Leo Adamek <leo@adamek.me>
2014-03-06 14:42:58 +0000

    Adding skeleton code

---
+++ plugins/system/segmentio/segmentio.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Segment.IO Integration for Joomla
+ *
+ * @package MrZen\Integraiton\Tracking
+ * @license GNU GPL v3
+ * @since 3.2
+ */
+
+
+jimport('joomla.plugin.plugin');
+jimport('segmentio.analytics');
+
+class plgSystemSegmentIO extends JPlugin {
+
+
+}
+
+
